e999ecfb087b3788c7f58287305f50b7466427ec
Paweł Kapalla <pkapalla@xesenix.pl>
2013-11-07 07:52:39 +0100

    experiment02 fixed frame animation

--- experiment02/src/main/java/pl/xesenix/experiments/experiment02/Controller.java
+++ experiment02/src/main/java/pl/xesenix/experiments/experiment02/Controller.java
@@ -4,6 +4,7 @@ package pl.xesenix.experiments.experiment02;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import javafx.animation.AnimationTimer;
@@ -44,18 +45,108 @@ public class Controller
 		Canvas canvas = new Canvas(800, 600);
 
 		final GraphicsContext gc = canvas.getGraphicsContext2D();
+		int fps = 2;
 
-		(new AnimationTimer() {
-
+		AnimationTimer animator = (new FixedStepAnimationTimer(fps) {
+			private FPS fps = new FPS();
+			
 			@Override
-			public void handle(long now)
+			public void render(long stepMiliseconds)
 			{
+				fps.update();
+				
 				gc.clearRect(0, 0, 800, 600);
-				gc.fillText(String.format("current time %d", now), 10, 50);
+				
+				gc.fillText(String.format("animation fps %d", fps.fps), 10, 20);
+				gc.fillText(String.format("frame fps %.2f", 1000 / (float)stepMiliseconds), 10, 40);
+				gc.fillText(String.format("step time %.3f", stepMiliseconds / 1000f), 10, 60);
 			}
-
-		}).start();
+			
+			@Override
+			public void start()
+			{
+				fps.start();
+				super.start();
+			}
+		});
+		
+		animator.start();
 
 		view.getChildren().add(canvas);
+	}
+	
+	
+	public abstract class FixedStepAnimationTimer extends AnimationTimer
+	{
+		private final long step;
+
+
+		private long lastAnimationFrameTime;
+
+
+		public FixedStepAnimationTimer(long fps)
+		{
+			this.step = 1000 / fps;
+		}
+
+
+		public void handle(long now)
+		{
+			long currentFrameTime = System.nanoTime() / 1000000;
+			long interval = currentFrameTime - lastAnimationFrameTime;
+			
+			if (interval >= step)
+			{
+				render(step);
+				
+				lastAnimationFrameTime = currentFrameTime - interval + step; // time overflow control
+			}
+		}
+
+
+		public abstract void render(long stepMiliseconds);
+
+
+		@Override
+		public void start()
+		{
+			lastAnimationFrameTime = System.nanoTime() / 1000000;
+			
+			super.start();
+		}
+	}
+
+
+	public static class FPS {
+		public int fps;
+
+		private int mesurements;
+		
+		private long lastFpsCalculationTime;
+		
+		
+		public void update()
+		{
+			update(System.nanoTime() / 1000000);
+		}
+		
+		public void update(long currentFrameTime)
+		{
+			mesurements ++;
+			
+			if (currentFrameTime - lastFpsCalculationTime > 1000)
+			{
+				log.debug("fps:{} interval: {}", fps, currentFrameTime - lastFpsCalculationTime);
+				fps = mesurements;
+				lastFpsCalculationTime = currentFrameTime;
+				mesurements = 0;
+			}
+		}
+		
+		public void start()
+		{
+			lastFpsCalculationTime = System.nanoTime() / 1000000;
+			mesurements = 0;
+		}
 	}
 }
