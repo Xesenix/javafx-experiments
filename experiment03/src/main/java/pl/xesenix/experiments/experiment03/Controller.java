7d2df51fc3d602ec38bb6d0178395093dff2a9bb
Paweł Kapalla <pkapalla@xesenix.pl>
2013-11-18 02:35:47 +0100

    experiment03 simple graphs nodes

--- experiment03/src/main/java/pl/xesenix/experiments/experiment03/Controller.java
+++ experiment03/src/main/java/pl/xesenix/experiments/experiment03/Controller.java
@@ -1,20 +1,30 @@
 
 package pl.xesenix.experiments.experiment03;
 
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pl.xesenix.experiments.experiment03.components.viewport.Viewport;
+import edu.uci.ics.jung.algorithms.layout.AggregateLayout;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 
 public class Controller
@@ -42,6 +52,9 @@ public class Controller
 	private TextArea console;
 
 
+	private HashMap<String, NodeView> map = new HashMap<String, NodeView>();
+
+
 	private Graph<String, String> graph;
 
 
@@ -53,20 +66,12 @@ public class Controller
 		assert console != null : "fx:id=\"console\" was not injected: check your FXML file 'app.fxml'.";
 		assert view != null : "fx:id=\"view\" was not injected: check your FXML file 'app.fxml'.";
 		assert viewport != null : "fx:id=\"viewport\" was not injected: check your FXML file 'app.fxml'.";
-		
-		viewport.addNodeToCanvas(new Circle(30, Color.web("#def")));
 
-		String[] npcActions = new String[] {
-			"Welcome what do yo want to do?",
-			"I have nothing to sell.",
-			"I know someone who may help you."
-		};
-		
-		String[] playerActions = new String[] {
-			"I want to buy some things",
-			"I want to go for an adventure"
-		};
-		
+		String[] npcActions = new String[] { "Welcome what do yo want to do?", "I have nothing to sell.",
+			"I know someone who may help you." };
+
+		String[] playerActions = new String[] { "I want to buy some things", "I want to go for an adventure" };
+
 		graph = new SparseMultigraph<String, String>();
 
 		graph.addVertex(npcActions[0]);
@@ -77,6 +82,72 @@ public class Controller
 		graph.addEdge(playerActions[1], npcActions[0], npcActions[2]);
 
 		console.setText(graph.toString());
+
+		Layout<String, String> layout = new CircleLayout<String, String>(graph);
+
+		layout.setSize(new Dimension(800, 600));
+		layout.initialize();
+
+		log.debug("{}", layout.transform(npcActions[0]));
+		log.debug("{}", layout.transform(npcActions[1]));
+		log.debug("{}", layout.transform(npcActions[2]));
+
+		for (String string : npcActions)
+		{
+			viewport.addNodeToCanvas(getNodeFor(string, layout));
+		}
+
+		for (String string : playerActions)
+		{
+			viewport.addNodeToCanvas(getEdgeFor(string, layout));
+		}
 	}
 
+
+	private Node getNodeFor(String vertice, Layout<String, String> layout)
+	{
+		Point2D point = layout.transform(vertice);
+		NodeView pane;
+		
+		if (!map.containsKey(vertice))
+		{
+			pane = new NodeView(vertice);
+
+			map.put(vertice, pane);
+		}
+		else
+		{
+			pane = map.get(vertice);
+		}
+		
+		pane.setLayoutX(point.getX());
+		pane.setLayoutY(point.getY());
+
+		return pane;
+	}
+
+
+	private Node getEdgeFor(String vertice, Layout<String, String> layout)
+	{
+		Point2D point = layout.transform(vertice);
+
+		Node node = new Circle(point.getX(), point.getY(), 20);
+
+		return node;
+	}
+
+
+	public class NodeView extends Pane
+	{
+		public NodeView(String label)
+		{
+			TextArea text = new TextArea();
+
+			text.setPrefColumnCount(20);
+			text.setPrefRowCount(3);
+			text.setText(label);
+
+			getChildren().add(text);
+		}
+	}
 }
