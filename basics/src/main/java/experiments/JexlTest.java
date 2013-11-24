76b150ffb500befb823eda544d5a54d2303314e7
Paweł Kapalla <pkapalla@xesenix.pl>
2013-11-24 23:47:45 +0100

    basics - simple scripting with jexl

---
+++ basics/src/main/java/experiments/JexlTest.java
@@ -0,0 +1,102 @@
+
+package experiments;
+
+import org.apache.commons.jexl2.JexlEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class JexlTest
+{
+	private static final Logger log = LoggerFactory.getLogger(JexlTest.class);
+
+
+	public static void main(String args[]) throws Exception
+	{
+		Context context = new Context();
+		context.setText("Hello world of scripting!!!");
+		context.setNumber(42);
+		
+		Context childContext = new Context();
+		childContext.setText("I can divide my parent number!");
+		childContext.setNumber(7);
+		
+		context.setContext(childContext);
+
+		try
+		{
+			queryProperty(context, "text");
+			queryProperty(context, "number");
+			queryProperty(context, "context");
+			queryProperty(context, "context.text");
+			queryProperty(context, "context.number");
+			queryProperty(context, "context.context");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+	private static void queryProperty(Context context, String query)
+	{
+		JexlEngine jexl = new JexlEngine();
+		
+		log.debug("{}: {}", query, jexl.getProperty(context, query));
+	}
+
+
+	public static class Context
+	{
+		private Context context;
+
+
+		private String text;
+
+
+		private Integer number;
+
+
+		public Context getContext()
+		{
+			return context;
+		}
+
+
+		public void setContext(Context context)
+		{
+			this.context = context;
+		}
+
+
+		public String getText()
+		{
+			return text;
+		}
+
+
+		public void setText(String text)
+		{
+			this.text = text;
+		}
+
+
+		public Integer getNumber()
+		{
+			return number;
+		}
+
+
+		public void setNumber(Integer number)
+		{
+			this.number = number;
+		}
+
+		
+		public String toString()
+		{
+			return String.format("{text: %s; number: %d; child: %s}", text, number, context);
+		}
+	}
+}
---
+++ basics/src/main/java/experiments/XmlSerializationTest.java
@@ -0,0 +1,439 @@
+
+package experiments;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.jexl2.Expression;
+import org.apache.commons.jexl2.JexlContext;
+import org.apache.commons.jexl2.JexlEngine;
+import org.apache.commons.jexl2.MapContext;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class XmlSerializationTest
+{
+	private static final Logger log = LoggerFactory.getLogger(XmlSerializationTest.class);
+	
+	
+	public static void main(String args[]) throws Exception
+	{
+		log.debug("started");
+		
+		Context context = new Context();
+		
+		Any<Context> alternative = new Any<Context>();
+		All<Context> coniunction = new All<Context>();
+		coniunction.add(new True());
+		coniunction.add(new True());
+		coniunction.add(new Equal<Context>(new PropertyGetter<Context>("x"), new PropertyGetter<Context>("y")));
+		coniunction.add(new True());
+		coniunction.add(new False());
+		alternative.add(coniunction);
+		alternative.add(new False());
+		alternative.add(new False());
+		
+		alternative.setContext(context);
+		
+		log.debug("condition: {}", StringEscapeUtils.unescapeHtml4(alternative.toString()));
+		log.debug("condition evaluation: {}", alternative.check());
+		
+		log.debug("condition as xml:\n{}", serializeObjectToXmlString(alternative));
+	}
+	
+	
+	public static String serializeObjectToXmlString(Object object)
+	{
+		XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
+		Element root;
+		
+		if (object instanceof IXmlSerializable)
+		{
+			root = ((IXmlSerializable) object).toXml();
+		}
+		else
+		{
+			root = new Element("unknown:" + object.getClass().getCanonicalName());
+		}
+		
+		return xmlOutput.outputString(root);
+	}
+	
+	
+	public static class Context
+	{
+		private Double x = (double) 10f;
+
+
+		public Double getY() {
+			return (double) 15f;
+		}
+
+
+		public Double getX()
+		{
+			return x;
+		}
+
+
+		public void setX(Double x)
+		{
+			this.x = x;
+		}
+	}
+
+
+	public static class PropertyGetter<T> implements IGetter<T>
+	{
+		private T bean;
+		
+		
+		private String selector;
+		
+		
+		public PropertyGetter(String selector)
+		{
+			this.selector = selector;
+		}
+
+
+		public void setContext(T context)
+		{
+			this.bean = context;
+		}
+		
+		
+		public Element toXml()
+		{
+			Element root = new Element("property");
+			
+			root.setAttribute("selector", selector);
+			
+			return root;
+		}
+		
+		
+		public Object get()
+		{
+			JexlEngine jexl = new JexlEngine();
+			
+			try {
+				if (bean != null)
+				{
+					return jexl.getProperty(bean, selector);
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+		
+		
+		public String toString()
+		{
+			return String.format("%s", get());
+		}
+	}
+	
+	
+	public static class True implements ICondition
+	{
+		public Boolean check()
+		{
+			return true;
+		}
+		
+		
+		public Element toXml()
+		{
+			Element root = new Element("true");
+			
+			return root;
+		}
+		
+		
+		public String toString()
+		{
+			return "true";
+		}
+	}
+	
+	
+	public static class False implements ICondition
+	{
+		public Boolean check()
+		{
+			return false;
+		}
+		
+		
+		public Element toXml()
+		{
+			Element root = new Element("false");
+			
+			return root;
+		}
+		
+		
+		public String toString()
+		{
+			return "false";
+		}
+	}
+	
+	
+	public static class Equal<T> implements ICondition, IContextDependant<T>
+	{
+		private IGetter<T> subject;
+		
+		
+		private IGetter<T> target;
+		
+		
+		private T context;
+		
+		
+		public Equal(IGetter<T> subject, IGetter<T> target)
+		{
+			this.subject = subject;
+			this.target = target;
+		}
+		
+		
+		public void setContext(T context)
+		{
+			this.context = context;
+			
+			subject.setContext(context);
+			target.setContext(context);
+		}
+		
+		
+		public Boolean check()
+		{
+			subject.setContext(context);
+			
+			if (target != null)
+			{
+				return target.equals(subject.get());
+			}
+			
+			return subject.get() == null;
+		}
+		
+		
+		public Element toXml()
+		{
+			Element root = new Element("equal");
+			
+			if (this.subject != null)
+			{
+				root.addContent(new Element("subject").setContent(this.subject.toXml()));
+			}
+
+			if (this.target != null)
+			{
+				root.addContent(new Element("target").setContent(this.target.toXml()));
+			}
+			
+			return root;
+		}
+		
+		
+		public String toString()
+		{
+			return this.subject.toString() + " = " + this.target.toString();
+		}
+	}
+
+
+	public static class Any<T> implements ICompositeCondition, IContextDependant<T>
+	{
+		ArrayList<ICondition> conditions = new ArrayList<ICondition>();
+		
+		
+		private T context;
+		
+		
+		public void add(ICondition condition)
+		{
+			conditions.add(condition);
+		}
+		
+		
+		public void setContext(T context)
+		{
+			this.context = context;
+			
+			for (ICondition condition : conditions)
+			{
+				if (condition instanceof IContextDependant)
+				{
+					((IContextDependant<T>) condition).setContext(context);
+				}
+			}
+		}
+		
+		
+		public Boolean check()
+		{
+			for (ICondition condition : conditions)
+			{
+				if (condition instanceof IContextDependant)
+				{
+					((IContextDependant<T>) condition).setContext(context);
+				}
+				
+				if (condition.check())
+				{
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		
+		public Element toXml()
+		{
+			Element root = new Element("any");
+			
+			for (ICondition condition : conditions)
+			{
+				root.addContent(condition.toXml());
+			}
+			
+			return root;
+		}
+		
+		
+		public String toString()
+		{
+			StringBuilder builder = new StringBuilder();
+			String delim = "";
+			
+			for (ICondition condition : conditions)
+			{
+				builder.append(delim + condition.toString());
+				delim = " &or; ";
+			}
+			
+			return "( " + builder.toString() + " )";
+		}
+	}
+	
+	
+	public static class All<T> implements ICompositeCondition, IContextDependant<T>
+	{
+		ArrayList<ICondition> conditions = new ArrayList<ICondition>();
+		
+		
+		private T context;
+		
+		
+		public void add(ICondition condition)
+		{
+			conditions.add(condition);
+		}
+		
+		
+		public void setContext(T context)
+		{
+			this.context = context;
+			
+			for (ICondition condition : conditions)
+			{
+				if (condition instanceof IContextDependant)
+				{
+					((IContextDependant<T>) condition).setContext(context);
+				}
+			}
+		}
+		
+		
+		public Boolean check()
+		{
+			for (ICondition condition : conditions)
+			{
+				if (condition instanceof IContextDependant)
+				{
+					((IContextDependant<T>) condition).setContext(context);
+				}
+				
+				if (!condition.check())
+				{
+					return false;
+				}
+			}
+			
+			return true;
+		}
+		
+		
+		public Element toXml()
+		{
+			Element root = new Element("all");
+			
+			for (ICondition condition : conditions)
+			{
+				root.addContent(condition.toXml());
+			}
+			
+			return root;
+		}
+		
+		
+		public String toString()
+		{
+			StringBuilder builder = new StringBuilder();
+			String delim = "";
+			
+			for (ICondition condition : conditions)
+			{
+				builder.append(delim + condition.toString());
+				delim = " &and; ";
+			}
+			
+			return "( " + builder.toString() + " )";
+		}
+	}
+	
+	
+	public interface IXmlSerializable
+	{
+		Element toXml();
+	}
+	
+	
+	public interface ICondition extends IXmlSerializable
+	{
+		Boolean check();
+	}
+	
+	
+	public interface ICompositeCondition extends ICondition
+	{
+		void add(ICondition condition);
+	}
+	
+	
+	public interface IContextDependant<T> extends IXmlSerializable
+	{
+		void setContext(T context);
+	}
+	
+	
+	public interface IGetter<T> extends IContextDependant<T>
+	{
+		Object get();
+	}
+}
